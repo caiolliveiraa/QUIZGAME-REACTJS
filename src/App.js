2fc5c2ab7db828c9b2f5d7225c95934d726bc404
Caio Oliveira <caiohenrique3335@gmail.com>
2023-12-13 17:34:34 -0300

    quiz-app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from 'react-router-dom'
-import Settings from './pages/Settings'
-import Questions from './pages/Questions'
-import FinalScreen from './pages/FinalScreen'
-import { Container } from '@mui/material';
-import { Box } from '@mui/material';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Settings from "./pages/Settings";
+import Questions from "./pages/Questions";
+import FinalScreen from "./pages/FinalScreen";
+import { Container, Typography } from "@mui/material";
+import { Box } from "@mui/system";
 
 function App() {
   return (
@@ -16,7 +12,9 @@ function App() {
         <Box textAlign="center" mt={5}>
           <Switch>
             <Route path="/" exact>
-              <TypeError variant="h2" fontWeight="bold">Quiz App</TypeError>
+              <Typography variant="h2" fontWeight="bold">
+                Quiz App
+              </Typography>
               <Settings />
             </Route>
             <Route path="/questions">
---
+++ src/pages/Questions.js
@@ -0,0 +1,10 @@
+
+const Questions = () => {
+    return (
+        <div>
+            Questoes
+        </div>
+    )
+}
+
+export default Questions
